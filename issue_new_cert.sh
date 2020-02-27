f6471880f260a0c3ff3c5511a7fd7b07d9dd10b1
Daniel Reuter <dreuter@kth.se>
2020-02-27 01:50:07 +0100

    upd

--- issue_new_cert.sh
+++ issue_new_cert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Execution timestamp: $(date)"
 THISPW=$(cat $HOME/.pw)
 HTML_DIR=/var/dockerdata/www_data
 LOCALVOL=/var/lib/docker/volumes/certs/_data
@@ -8,10 +9,11 @@ DOMAIN=rojter.tech
 LOCAL=$LOCALVOL/$DOMAIN
 TARGET=$TARGETVOL/$DOMAIN
 
-docker run -it --rm \
-    -v certs:/acme.sh \
-    -v $HTML_DIR:/www-data \
-    rojtertech/acme --issue \
+echo "Initializing docker engine to renew cert ..."
+docker run -i --rm \
+	-v certs:/acme.sh \
+    	-v $HTML_DIR:/www-data \
+    	rojtertech/acme --issue \
 	-d $DOMAIN \
 	-d code.$DOMAIN \
 	-d dns.$DOMAIN \
@@ -19,19 +21,22 @@ docker run -it --rm \
 	-d www.$DOMAIN \
 	-w /www-data
 
-sleep 10
+sleep 30
 sudo mkdir -p $TARGET
+sudo chmod 777 $TARGET
+sudo chown $USER:$USER $TARGET
 sudo cp -r $LOCAL $TARGETVOL
 sudo chmod 777 $TARGET
 sudo find $TARGET -exec sudo chmod 777 {} \;
+sudo find $TARGET -exec sudo chown $USER:$USER {} \;
 
-mv $TARGET/ca.cer $TARGET/chain.pem
-mv $TARGET/$DOMAIN.conf $TARGET/domain.conf
-mv $TARGET/$DOMAIN.csr.conf $TARGET/domain.csr.conf
-mv $TARGET/fullchain.cer $TARGET/fullchain.pem
-mv $TARGET/$DOMAIN.cer $TARGET/cert.pem
-mv $TARGET/$DOMAIN.csr $TARGET/domain.csr
-mv $TARGET/$DOMAIN.key $TARGET/privkey.pem
+sudo mv $TARGET/ca.cer $TARGET/chain.pem
+sudo mv $TARGET/$DOMAIN.conf $TARGET/domain.conf
+sudo mv $TARGET/$DOMAIN.csr.conf $TARGET/domain.csr.conf
+sudo mv $TARGET/fullchain.cer $TARGET/fullchain.pem
+sudo mv $TARGET/$DOMAIN.cer $TARGET/cert.pem
+sudo mv $TARGET/$DOMAIN.csr $TARGET/domain.csr
+sudo mv $TARGET/$DOMAIN.key $TARGET/privkey.pem
 
 sudo openssl pkcs12 -export -nodes \
 	-in $TARGET/fullchain.pem \
@@ -41,4 +46,5 @@ sudo openssl pkcs12 -export -nodes \
 	-passout pass:$THISPW
 
 sudo find $TARGET -exec sudo chmod 777 {} \;
+sudo find $TARGET -exec sudo chown $USER:$USER {} \;
 rsync -av $TARGET/* dreuter@debian.rojter.lo:$TARGET/
